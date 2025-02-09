1fb14871dac6d2ccbf0cdd6533c2409aaf5e3251
oluwagbemiga <oluwagbemigagbangbola@gmail.com>
2025-02-09 20:58:25 +0100

    init

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -2,7 +2,7 @@
 
 pragma solidity ^0.8.7;
 
-import {AggregatorV3Interface} from "@chainlink/contracts@1.2.0/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 import {PriceConverter} from "./PriceConverter.sol";
 
 contract FundMe {
